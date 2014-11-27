e2f18cb9dcdb5aa252a3651b6e91ff7441c87b1d
MrCrankHank <MrCrankHank@users.noreply.github.com>
2014-11-27 18:01:20 +0100

    Create check_megacli_status.sh

---
+++ check_megacli_status.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#User nagios executes the megacli binary, add this to your /etc/sudoers file
+#nagios ALL=(ALL) NOPASSWD:/opt/MegaRAID/MegaCli/MegaCli64
+
+
+megacli="sudo /opt/MegaRAID/MegaCli/MegaCli64"
+
+name=$($megacli -LDInfo -L$1 -aALL | grep -e "^Name" | tr -d ':' | tr -d ' ' | sed -e 's/Name//g')
+state=$($megacli -LDInfo -L$1 -aALL | grep -e "^State" | tr -d ':' | tr -d ' ' | sed -e 's/State//g')
+size=$($megacli -LDInfo -L$1 -aALL | grep -e "^Size")
+
+if [ "$state" == "Optimal" ]
+then
+	nagioscode=0
+else
+	nagioscode=2
+fi
+
+echo "${name}: $state"
+exit $nagioscode
